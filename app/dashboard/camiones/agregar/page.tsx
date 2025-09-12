ac85b670f492da5765308c660ea0a3fb179dad59
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-12 03:23:23 -0300

    Se crea la vista para agregar, editar y eliminar un camion

---
+++ app/dashboard/camiones/agregar/page.tsx
@@ -0,0 +1,26 @@
+import CreateCamionForm from '@/app/components/camiones/create-form';
+import Breadcrumbs from '@/app/components/dashboard/breadcrums';
+import { Metadata } from 'next';
+
+export const metadata:Metadata = {
+  title: 'Agregar'
+};
+ 
+export default async function Page () {
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Camiones', href: '/dashboard/camiones' },
+          {
+            label: 'Agregar camión',
+            href: '/dashboard/camiones/agregar',
+            active: true,
+          },
+        ]}
+      />
+      <CreateCamionForm />
+    </main>
+  );
+}
