335a227d6939de70b7c0e8693f845b3de078244e
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-13 02:47:29 -0300

    Se crea vista para agregar un tipo de camion

--- app/dashboard/camiones/page.tsx
+++ app/dashboard/camiones/page.tsx
@@ -1,8 +1,10 @@
 import CamionesTable from "@/app/components/camiones/camionesTable";
 import { AddCamion } from "@/app/components/camiones/buttons";
-import { CamionesTableSkeleton } from "@/app/components/dashboard/skeletons";
+import { CamionesTableSkeleton, TableRowSkeleton } from "@/app/components/dashboard/skeletons";
 import { Metadata } from "next";
 import { Suspense } from "react";
+import TiposCamionTable from "@/app/components/camiones/tiposCamionTable";
+import { AddTipoCamionBtn } from "@/app/components/camiones/addTipoCamionBtn";
 
 
 export const metadata: Metadata = {
@@ -21,6 +23,15 @@ export default function Camiones () {
       {<Suspense fallback={<CamionesTableSkeleton />}>
         <CamionesTable />
       </Suspense>}
+      <div className="flex mt-8 w-full items-center justify-between">
+        <h1 className={'text-2xl'}>Tipos de camión</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <AddTipoCamionBtn />
+      </div>
+      {<Suspense>
+        <TiposCamionTable />
+      </Suspense>}      
     </div>
   );
 }
