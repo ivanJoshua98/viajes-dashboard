7824fc03180906f88829f87a25e85776ca8b2c08
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-16 02:50:36 -0300

    Se crea la vista para agregar una nueva tarifa

---
+++ app/dashboard/tarifas/[id]/editar/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page () {
+  return (
+    <>
+    Editar tarifa
+    </>
+  )
+}
---
+++ app/dashboard/tarifas/agregar/page.tsx
@@ -0,0 +1,33 @@
+import Breadcrumbs from '@/app/components/dashboard/breadcrums';
+import CreateTarifaForm from '@/app/components/tarifas/createTarifaForm';
+import { fetchTiposCamion } from '@/app/lib/data/fetchDataCamiones';
+import { fetchZonas } from '@/app/lib/data/fetchDataZonas';
+import { Metadata } from 'next';
+
+export const metadata:Metadata = {
+  title: 'Agregar'
+};
+ 
+export default async function Page () {
+
+  const [zonas, tipos] = await Promise.all([
+    fetchZonas(),
+    fetchTiposCamion(),
+  ]);
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Tarifas', href: '/dashboard/tarifas' },
+          {
+            label: 'Agregar tarifa',
+            href: '/dashboard/tarifa/agregar',
+            active: true,
+          },
+        ]}
+      />
+      <CreateTarifaForm zonas={zonas} tipos={tipos}/>
+    </main>
+  );
+}
