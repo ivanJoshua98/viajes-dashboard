454db03c7112856f51220c95efcbeedf22bae584
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-15 23:38:07 -0300

    Se crean funciones para listar las tarifas

--- app/lib/data/definitions.ts
+++ app/lib/data/definitions.ts
@@ -20,3 +20,12 @@ export type Zona = {
   nombre: string;
   region: string;
 };
+
+export type Tarifa = {
+  id: string;
+  zona_id: string;
+  tipo_camion_id: string;
+  monto_centavos: number;
+  tipo: string;
+  zona_nombre: string;
+};
