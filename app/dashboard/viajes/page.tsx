837e3a17897964cec9c089087a467af6d7c638df
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-19 19:52:09 -0300

    Se crea la vista para listar y agregar viajes

--- app/dashboard/viajes/page.tsx
+++ app/dashboard/viajes/page.tsx
@@ -1,6 +1,26 @@
+import { CamionesTableSkeleton } from "@/app/components/dashboard/skeletons";
+import { AddViaje } from "@/app/components/viajes/buttons";
+import ViajesTable from "@/app/components/viajes/viajesTable";
+import { Metadata } from "next"
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: 'Viajes'
+};
 
 export default function Viajes () {
+  
   return (
-    <h1>Viajes</h1>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={'text-2xl'}>Viajes</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <AddViaje />
+      </div>
+      {<Suspense fallback={<CamionesTableSkeleton />}>
+        <ViajesTable />
+      </Suspense>}
+    </div>
   )
 }
