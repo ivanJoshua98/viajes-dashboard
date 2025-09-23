c0fdc766e1f611c20ed97f6be926b7d6ab375d55
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-09-22 21:57:38 -0300

    Se agrega funcionalidad para editar un viaje

--- app/lib/actions/viajeAction.ts
+++ app/lib/actions/viajeAction.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import sql from "@/app/lib/data/db";
 import { ViajeFormSchema } from "@/app/lib/schemas/viajeFormSchema";
 import { success } from "zod/v4";
+import { redirect } from "next/navigation";
 
 export type StateViajeForm = {
   errors? : {
@@ -119,6 +120,92 @@ export async function createViaje (prevState: StateViajeForm, formData: FormData
     success: true
   };
 }
+
+
+ /*--------------------------------UPDATE--------------------------------------------------*/
+
+
+const UpdateViaje = ViajeFormSchema.omit({ viaje_id: true });
+  
+export async function updateViaje (id: string, prevState: StateViajeForm, formData: FormData) {
+  const validatedFields = UpdateViaje.safeParse({
+    fecha: formData.get('fecha'),
+    zona: formData.get('zona'),
+    tipo_camion: formData.get('tipo'),
+    cajones: formData.get('cajones'),
+    cant_clientes: formData.get('cant-clientes'),
+    monto: formData.get('monto'),
+    observaciones: formData.get('observaciones'),
+    camion: formData.get('camion'),
+    litros_combustible: formData.get('litros'),
+    kilometraje: formData.get('km'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Faltan completar campos. No se pudo editar el viaje'
+    }
+  }
+
+  const {
+    fecha,
+    zona,
+    tipo_camion,
+    cajones,
+    cant_clientes,
+    monto,
+    observaciones,
+    camion,
+    litros_combustible,
+    kilometraje,
+  } = validatedFields.data;
+
+  try {
+    const result = await sql`
+      SELECT * FROM viajes
+      WHERE fecha = ${fecha} AND 
+      fecha = ${fecha} AND
+      tipo_camion = ${tipo_camion} AND
+      camion = ${camion};
+    `;
+    if (result.length > 0) {
+      return {
+        message: 'Ya existe un viaje con la fecha, la zona, el tipo de camión y la patente asociadas'
+      };
+    }
+  } catch (error) {
+    console.log('Database error:', error);
+    return  {
+      message: 'Database error: No se pudo agregar el viaje.'
+    };
+  }
+
+  try {
+     await sql`
+      UPDATE viajes  
+      SET fecha = ${fecha},
+          zona = ${zona}, 
+          tipo_camion = ${tipo_camion}, 
+          cajones = ${cajones}, 
+          cant_clientes = ${cant_clientes}, 
+          valor_flete_centavos = ${monto * 100}, 
+          observaciones = ${observaciones || ''}, 
+          camion = ${camion}, 
+          litros_combustible = ${litros_combustible}, 
+          kilometraje = ${kilometraje}
+      WHERE id = ${id};
+    `; 
+  } catch (error) {
+    return {
+      message: 'Database Error: No se pudo editar la viaje.',
+    };
+  }
+   
+  revalidatePath('/dashboard/viajes');
+  redirect('/dashboard/viajes');
+ }
+
 
 /*--------------------------------DELETE--------------------------------------------------*/
 
--- app/lib/data/fetchDataViajes.ts
+++ app/lib/data/fetchDataViajes.ts
@@ -30,3 +30,36 @@ export async function fetchViajes () {
     throw new Error('Error al obtener los viajes');
   }
 }
+
+
+export async function fetchViajeById (id: string) {
+  try {
+    const [ viaje ] = await sql<Viaje[]>`
+      SELECT viajes.id AS viaje_id, 
+             fecha, 
+             zona AS zona_id, 
+             zonas.nombre AS zona_nombre, 
+             tipo_camion AS tipo_id, 
+             tipos_camion.tipo AS tipo_camion_nombre, 
+             cajones, 
+             cant_clientes, 
+             valor_flete_centavos, 
+             observaciones, 
+             camion AS camion_id, 
+             camiones.patente AS camion_patente, 
+             litros_combustible, 
+             kilometraje 
+      FROM viajes 
+      JOIN zonas ON viajes.zona = zonas.id 
+      JOIN tipos_camion ON viajes.tipo_camion = tipos_camion.id 
+      JOIN camiones ON viajes.camion = camiones.id 
+      WHERE viajes.id = ${id};
+    `;  
+    return viaje;
+    } catch (error) {
+    console.log('Database error:', error);
+    throw new Error('Error al obtener el viaje.');
+  }
+  
+
+}
