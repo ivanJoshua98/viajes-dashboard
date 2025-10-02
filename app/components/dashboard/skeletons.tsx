84f8b00ef8451125566e041c2c87b75204f9c018
ivanJoshua98 <ivan.sanmartin98@gmail.com>
2025-10-02 00:43:33 -0300

    Se corrigen los skeletons de la pagina inicio

--- app/components/dashboard/skeletons.tsx
+++ app/components/dashboard/skeletons.tsx
@@ -97,14 +97,14 @@ const shimmer =
 export function CardSkeleton() {
   return (
     <div
-      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-100 p-2 shadow-sm`}
+      className={`${shimmer} relative overflow-hidden rounded-xl bg-gray-800 p-2 shadow-sm`}
     >
       <div className="flex p-4">
-        <div className="h-5 w-5 rounded-md bg-gray-200" />
-        <div className="ml-2 h-6 w-16 rounded-md bg-gray-200 text-sm font-medium" />
+        <div className="h-5 w-5 rounded-md bg-gray-500" />
+        <div className="ml-2 h-6 w-16 rounded-md bg-gray-500 text-sm font-medium" />
       </div>
-      <div className="flex items-center justify-center truncate rounded-xl bg-white px-4 py-8">
-        <div className="h-7 w-20 rounded-md bg-gray-200" />
+      <div className="flex items-center justify-center truncate rounded-xl bg-gray-600 px-4 py-8">
+        <div className="h-7 w-20 rounded-md bg-gray-500" />
       </div>
     </div>
   );
@@ -126,13 +126,13 @@ export function ViajeSkeleton() {
   return (
     <div className="flex flex-row items-center justify-between border-b border-gray-100 py-4">
       <div className="flex items-center">
-        <div className="mr-2 h-8 w-8 rounded-full bg-gray-200" />
+        <div className="mr-2 h-8 w-8 rounded-full bg-gray-500" />
         <div className="min-w-0">
-          <div className="h-5 w-40 rounded-md bg-gray-200" />
-          <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+          <div className="h-5 w-40 rounded-md bg-gray-500" />
+          <div className="mt-2 h-4 w-12 rounded-md bg-gray-500" />
         </div>
       </div>
-      <div className="mt-2 h-4 w-12 rounded-md bg-gray-200" />
+      <div className="mt-2 h-4 w-12 rounded-md bg-gray-500" />
     </div>
   );
 }
@@ -142,9 +142,9 @@ export function LatestViajesSkeleton() {
     <div
       className={`${shimmer} relative flex w-full flex-col overflow-hidden md:col-span-4`}
     >
-      <div className="mb-4 h-8 w-36 rounded-md bg-gray-100" />
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-100 p-4">
-        <div className="bg-white px-6">
+      <div className="mb-4 h-8 w-36 rounded-md bg-gray-500" />
+      <div className="flex grow flex-col justify-between rounded-xl bg-gray-800 p-4">
+        <div className="bg-gray-600 px-6">
           <ViajeSkeleton />
           <ViajeSkeleton />
           <ViajeSkeleton />
@@ -152,8 +152,8 @@ export function LatestViajesSkeleton() {
           <ViajeSkeleton />
         </div>
         <div className="flex items-center pb-2 pt-6">
-          <div className="h-5 w-5 rounded-full bg-gray-200" />
-          <div className="ml-2 h-4 w-20 rounded-md bg-gray-200" />
+          <div className="h-5 w-5 rounded-full bg-gray-500" />
+          <div className="ml-2 h-4 w-20 rounded-md bg-gray-500" />
         </div>
       </div>
     </div>
